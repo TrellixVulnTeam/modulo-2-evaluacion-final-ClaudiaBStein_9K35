26fd1f04c2fcd59cd7611f8607b3558a06367dc6
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-10 12:57:27 +0200

    api

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,4 +1,13 @@
 'use strict';
 
+const input = document.querySelector('.js-search-input');
+const menu = document.querySelector('.js-menu-list');
 
+function addApi() {
+  let inputValue = input.value;
+  let api = `//api.tvmaze.com/search/shows?q=${inputValue}`;
+  return api;
+}
+
+addApi();
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 'use strict';
 
+const input = document.querySelector('.js-search-input');
+const menu = document.querySelector('.js-menu-list');
+
+function addApi() {
+  let inputValue = input.value;
+  let api = `//api.tvmaze.com/search/shows?q=${inputValue}`;
+  return api;
+}
+
+addApi();
