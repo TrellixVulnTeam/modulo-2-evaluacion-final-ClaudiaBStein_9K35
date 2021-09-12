fd996506e446fd9f076fdcc27fb821b8072742e7
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-12 19:24:28 +0200

    fetch added for array and function and event for button

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,36 +1,44 @@
 'use strict';
 
 const showsContainer = document.querySelector('.js-shows');
-const input = document.querySelector('js-search-input');
+const input = document.querySelector('.js-search-input');
+const button = document.querySelector('.js-button');
 let arrayShows = [];
 
 
 function paintArrayShows() {
   let html='';
-  //info es cada elemento del array
-  for (const info of arrayShows);{
+  //info es cada elemento del array de 
+  for (const info of arrayShows){
     console.log(info);
     html += `<li class="js-show show">`;
-    html += `<h5>Título de la serie</h5>`
+    html += `<h5>Título de la serie</h5>`;
     html += `</li>`;
+    html += `<img class="show-image js-image" src="${show.image}" alt="" />`;
   }
   showsContainer.innerHTML = html;
 }
 
-fetch(
-  `//api.tvmaze.com/search/shows?q=${shows}`
-)
-.then((response) => response.json())
-.then((data) => {
-    arrayShows = data;
-    paintArrayShows();
+function handleSearch (ev){
+
+  ev.preventDefault();
+  const inputValue = input.value;
+
+  fetch(
+    `//api.tvmaze.com/search/shows?q=${inputValue}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      arrayShows = data;
     
-}); 
+      // paintArrayShows();
+    
+    });
 
+}
 
-
-
-
+button.addEventListener ('click' , handleSearch);
+console.log(arrayShows);
 
 
 // const input = document.querySelector('.js-search-input');
