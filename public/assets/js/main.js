f90067a906c7b49e9148ca19514999780bf40a2b
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-13 10:48:37 +0200

    listen to favorites

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,25 +1,60 @@
 'use strict';
 
 const showsContainer = document.querySelector('.js-shows');
+const favoritesContainer = document.querySelector('.js-favorites');
 const input = document.querySelector('.js-search-input');
 const button = document.querySelector('.js-button');
+let defaultImg = './assets/images/wc-placeholder.jpg';
 let arrayShows = [];
 let arrayShowsId = [];
 let arrayShowsName = [];
 let arrayShowsImg = [];
+let favorites = [];
 
 
+function handleShow(ev){
+  console.log(ev.currentTarget.id);
+}
+function handleFavorites(ev){
+  console.log(ev.currentTarget.id);
+}
 
+function listenFavorites() {
+  ev.preventDefault();
+  let listFavorites = document.querySelectorAll('.js-show');
+  for (let favoriteSelected of favoritesDisplayed){
+    favoriteSelected.addEventListener('click', handleFavorites);
+  }
+}
+
+// function paintFavorites() {
+//   let html = '';
+//   for (const favorite of favorites) {
+//     if (favorite.show.image === null) {
+//       html += `<li class="favorite js-favorite" id = "${favorite.show.id}">`;
+//       html += `<h2 class="favorite-name js-favorite-name">${favorite.show.name}</h2>`;
+//       html += `<img src="${defaultImg}" alt="">`;
+//       html += `</li>`;
+          
+//     } else {
+//       html += `<li class="favorite js-favorite" id = "${favorite.show.id}">;
+//       html += `<h2 class="favorite-name js-favorite-name">${favorite.show.name}"</h2>`;
+//       html += `<img src="${favorite.show.image.medium}" alt="">`;
+//       html += `</li>`;
+//     }
+//     paintFavorites();
+//   }
+  
+//   favoritesContainer.innerHTML = html;
+//   //setLocalStorage();
+// }
 
 function paintArrayShows() {
   let html='';
-  //info es cada elemento del array de 
   for (const info of arrayShows){
-    
     arrayShowsId = info.show.id;
     arrayShowsImg = info.show.image.medium;
     arrayShowsName = info.show.name;
-    console.log(info);
     html += `<li class="js-show show" id="${arrayShowsId}">`;
     html += `<h5>${arrayShowsName}</h5>`;
     html += `<img class="show-image js-image" src="${arrayShowsImg}" alt="${arrayShowsName}"/>`;
@@ -28,6 +63,7 @@ function paintArrayShows() {
   }
   showsContainer.innerHTML = html;
 }
+
 
 function handleSearch (ev){
 
@@ -50,7 +86,11 @@ function handleSearch (ev){
 
 button.addEventListener ('click' , handleSearch);
 
-
+// function listenFavoritesResult(){
+//   const selectedShow = parseInt(ev.currentTarget.id);
+//   const clickedShow = arrayShows.find((data) => {
+//     return data.show.id === selectedShow;
+// }
 
 // const input = document.querySelector('.js-search-input');
 // const menu = document.querySelector('.js-menu-list');
@@ -81,5 +121,5 @@ button.addEventListener ('click' , handleSearch);
 // }
 
 
-// document.querySelector('.js-form').addEventListener("submit", getShow);
+// document.querySelector('.js-form').addEventListener("submit", getShow)
 //# sourceMappingURL=main.js.map
