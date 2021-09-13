2f81d9ef21d785981af50e5cd92df22f460d9fe1
Claudia Stein <claudiasantanastein@hotmail.com>
2021-09-13 11:40:19 +0200

    class added to current target favorites

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,56 @@
-'use strict';
+"use strict";
 
-const showsContainer = document.querySelector('.js-shows');
-const favoritesContainer = document.querySelector('.js-favorites');
-const input = document.querySelector('.js-search-input');
-const button = document.querySelector('.js-button');
-let defaultImg = './assets/images/wc-placeholder.jpg';
+const showsContainer = document.querySelector(".js-shows");
+const favoritesContainer = document.querySelector(".js-favorites");
+const input = document.querySelector(".js-search-input");
+const button = document.querySelector(".js-button");
+let defaultImg = "./assets/images/wc-placeholder.jpg";
 let arrayShows = [];
 let arrayShowsId = [];
 let arrayShowsName = [];
 let arrayShowsImg = [];
 let favorites = [];
 
-
-function handleShow(ev){
+function handleShow(ev) {
   console.log(ev.currentTarget.id);
 }
-function handleFavorites(ev){
+function handleFavorites(ev) {
   console.log(ev.currentTarget.id);
+  console.log(ev.target);
+  const favSelected = ev.currentTarget;
+  favSelected.classList.toggle("favorite-identifier");
 }
 
 function listenFavorites() {
-  ev.preventDefault();
-  let listFavorites = document.querySelectorAll('.js-show');
-  for (let favoriteSelected of favoritesDisplayed){
-    favoriteSelected.addEventListener('click', handleFavorites);
+  let listFavorites = document.querySelectorAll(".js-show");
+  for (let favoriteSelected of listFavorites) {
+    favoriteSelected.addEventListener("click", handleFavorites);
   }
 }
 
-// function paintFavorites() {
-//   let html = '';
-//   for (const favorite of favorites) {
-//     if (favorite.show.image === null) {
-//       html += `<li class="favorite js-favorite" id = "${favorite.show.id}">`;
-//       html += `<h2 class="favorite-name js-favorite-name">${favorite.show.name}</h2>`;
-//       html += `<img src="${defaultImg}" alt="">`;
-//       html += `</li>`;
-          
-//     } else {
-//       html += `<li class="favorite js-favorite" id = "${favorite.show.id}">;
-//       html += `<h2 class="favorite-name js-favorite-name">${favorite.show.name}"</h2>`;
-//       html += `<img src="${favorite.show.image.medium}" alt="">`;
-//       html += `</li>`;
-//     }
-//     paintFavorites();
-//   }
-  
-//   favoritesContainer.innerHTML = html;
-//   //setLocalStorage();
-// }
+function paintFavorites() {
+  let html = "";
+  for (const favorite of favorites) {
+    if (favorite.show.image === null) {
+      html += `<li class="favorite js-favorite" id = "${favorite.show.id}">`;
+      html += `<h2 class="favorite-name js-favorite-name">${favorite.show.name}</h2>`;
+      html += `<img src="${defaultImg}" alt="">`;
+      html += `</li>`;
+    } else {
+      html += `<li class="favorite js-favorite" id = "${favorite.show.id}">`;
+      html += `<h2 class="favorite-name js-favorite-name">${favorite.show.name}"</h2>`;
+      html += `<img src="${favorite.show.image.medium}" alt="">`;
+      html += `</li>`;
+    }
+  }
+
+  favoritesContainer.innerHTML = html;
+  //setLocalStorage();
+}
 
 function paintArrayShows() {
-  let html='';
-  for (const info of arrayShows){
+  let html = "";
+  for (const info of arrayShows) {
     arrayShowsId = info.show.id;
     arrayShowsImg = info.show.image.medium;
     arrayShowsName = info.show.name;
@@ -59,32 +58,27 @@ function paintArrayShows() {
     html += `<h5>${arrayShowsName}</h5>`;
     html += `<img class="show-image js-image" src="${arrayShowsImg}" alt="${arrayShowsName}"/>`;
     html += `</li>`;
-    
   }
   showsContainer.innerHTML = html;
+
+  listenFavorites();
 }
 
-
-function handleSearch (ev){
-
+function handleSearch(ev) {
   ev.preventDefault();
   const inputValue = input.value;
 
-  fetch(
-    `//api.tvmaze.com/search/shows?q=${inputValue}`
-  )
+  fetch(`//api.tvmaze.com/search/shows?q=${inputValue}`)
     .then((response) => response.json())
     .then((data) => {
       arrayShows = data;
       console.log(arrayShows);
-    
-      paintArrayShows();
-    
-    });
 
+      paintArrayShows();
+    });
 }
 
-button.addEventListener ('click' , handleSearch);
+button.addEventListener("click", handleSearch);
 
 // function listenFavoritesResult(){
 //   const selectedShow = parseInt(ev.currentTarget.id);
@@ -102,8 +96,7 @@ button.addEventListener ('click' , handleSearch);
 //   return api;
 // }
 
-// addApi(); 
-
+// addApi();
 
 // function getShow(event) {
 //   console.log('hola');
@@ -119,6 +112,5 @@ button.addEventListener ('click' , handleSearch);
 //   };
 //   });
 // }
-
 
 // document.querySelector('.js-form').addEventListener("submit", getShow)
